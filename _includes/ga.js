2ff8950c51d529dd21d6eea5cc0b7df48a6a0805
Septolum <septolum@gmail.com>
2021-10-30 23:41:04 +0100

    cookie ssl fix

--- _includes/ga.js
+++ _includes/ga.js
@@ -2,7 +2,7 @@
   var ga = document.createElement("script");
   ga.type = "text/javascript";
   ga.async = true;
-  ga.src = "http://www.googletagmanager.com/gtag/js?id=UA-211608587-1";
+  ga.src = "https://www.googletagmanager.com/gtag/js?id=UA-211608587-1";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(ga, s);
 })();
